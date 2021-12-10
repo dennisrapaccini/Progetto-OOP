802a94fd804605790ab5652a4cafc753d258a815
dennisrapaccini <dennisrapaccini@gmail.com>
2021-12-11 00:05:18 +0100

    Aggiunto metodo getPostsfromCity

--- MetaStats/src/main/java/com/project/MetaStats/MetaStatsApplication.java
+++ MetaStats/src/main/java/com/project/MetaStats/MetaStatsApplication.java
@@ -3,23 +3,27 @@ package com.project.MetaStats;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.json.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.project.MetaStats.filtersManagement.FileManagement;
+import com.project.MetaStats.filtersManagement.FilterCity;
 import com.project.MetaStats.service.ServiceImpl;
 
 @SpringBootApplication
 public class MetaStatsApplication {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MetaStatsApplication.class, args);
 		ServiceImpl service = new ServiceImpl();
 		service.getPost_User();
 		service.getMessage_Post();
-		FileManagement file  = new FileManagement();
-		file.getFile();
+		JSONArray ciao = new JSONArray();
+		FilterCity filtercity= new FilterCity();
+		
+		ciao = filtercity.getPostsfromCity("Treia");
 	}
 	
 }
--- MetaStats/src/main/java/com/project/MetaStats/filtersManagement/FilterCity.java
+++ MetaStats/src/main/java/com/project/MetaStats/filtersManagement/FilterCity.java
@@ -1,24 +1,32 @@
 package com.project.MetaStats.filtersManagement;
+
 import java.util.ArrayList;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
 import com.project.MetaStats.model.Post;
 import com.project.MetaStats.service.ServiceImpl;
 
-public class FilterCity extends Filter{
-	
+public class FilterCity extends Filter {
+
 	ServiceImpl service = new ServiceImpl();
 	String prova = service.getMessage_Post();
+
+	public FilterCity() {
 	
+	}
+
 	private String city;
-	
+
 	public FilterCity(String city) {
 		super();
 		this.city = city;
 	}
-	
+
 	public String getCity() {
 		return city;
 	}
-	
+
 	public void setCity(String city) {
 		this.city = city;
 	}
@@ -27,8 +35,29 @@ public class FilterCity extends Filter{
 	public String filter() {
 		return city;
 	}
-	
-	
-	
-	
+
+	/**
+	 * Metodo che restituisce in JSONArray i post che contengono il parametro city.
+	 * 
+	 * @param Città da cercare
+	 * @return true se la città è presente tra i post, false altrimenti.
+	 */
+	public JSONArray getPostsfromCity(String city) throws Exception { // TODO Due eccezioni fare: uno se non è presente
+																		// city e un'altra per i JSONObject sotto.
+
+		ServiceImpl serviceImpl = new ServiceImpl();
+		JSONObject object = serviceImpl.getPost_User();
+		JSONObject object2 = object.getJSONObject("posts");
+		JSONArray array = object2.getJSONArray("data");
+		JSONArray ArrayPostsfromCity = new JSONArray();
+
+		for (int i = 0; i < array.length(); i++) {
+			String message = array.getJSONObject(i).getString("message");
+			if (message.contains(city)) {
+				ArrayPostsfromCity.put(array.getJSONObject(i));
+			}
+		}
+		System.out.println(ArrayPostsfromCity);
+		return ArrayPostsfromCity;
+	}
 }
