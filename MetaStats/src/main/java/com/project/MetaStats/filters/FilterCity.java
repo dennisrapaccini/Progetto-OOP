6dd3d0525f21f09022478f9365ab898d4ffc9d61
Huncho07 <93982318+Huncho07@users.noreply.github.com>
2021-12-10 11:01:51 +0100

    modifiche a sevice, serviceimpl e filter

---
+++ MetaStats/src/main/java/com/project/MetaStats/filters/FilterCity.java
@@ -0,0 +1,34 @@
+package com.project.MetaStats.filters;
+import java.util.ArrayList;
+import com.project.MetaStats.model.Post;
+import com.project.MetaStats.service.ServiceImpl;
+
+public class FilterCity extends Filter{
+	
+	ServiceImpl service = new ServiceImpl();
+	String prova = service.getMessage_Post();
+	
+	private String city;
+	
+	public FilterCity(String city) {
+		super();
+		this.city = city;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String filter() {
+		return city;
+	}
+	
+	
+	
+	
+}
